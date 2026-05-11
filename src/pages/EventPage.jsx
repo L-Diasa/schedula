15e665099de3739c32a4a1497c976c8256147b00
Anniken Strand <annikeqs@stud.ntnu.no>
2026-05-11 10:03:38 +0200

    ISSUE5 Refactor: Replace emoji icons with Heroicons

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,7 +1,7 @@
 import "./eventPage.css";
 
 export default function EventPage() {
-  const participants = [
+   const participants = [
     "John",
     "Emma",
     "Lucas",
@@ -40,8 +40,10 @@ export default function EventPage() {
 
             <div className="participants-row">
               {participants.slice(0, 7).map((participant, index) => (
-                <div className="participant-avatar" key={index}>
-                  👤
+                <div className="icon-card" key={index}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="hero-icon">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
                 </div>
               ))}
             </div>
@@ -62,7 +64,11 @@ export default function EventPage() {
         <aside className="event-sidebar">
 
           <div className="info-box">
-            <div className="icon-box">🕒</div>
+            <div className="icon-card">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="hero-icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </div>
 
             <p>
               Friday April 24 2026,
@@ -72,7 +78,12 @@ export default function EventPage() {
           </div>
 
           <div className="info-box">
-            <div className="icon-box">📍</div>
+            <div className="icon-card">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="hero-icon">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+                </svg>
+            </div>
 
             <p>
               Campus SophiaTech,
