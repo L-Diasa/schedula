e68ea23b0838a7e43ea8e97c8ea6624336d49a74
Johanne GG <johanne.gjedrem@icloud.com>
2026-05-13 09:02:56 +0200

    ISSUE2 Feat: Create useFetch hook

---
+++ src/utils/hooks/useFetch.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+export function useFetch(url) {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('Error fetching users data');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                });
+        }, 1000);
+    }, [url]);
+
+    return { data, isPending, error };
+}
