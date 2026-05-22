b47589c168e5a1f3d9f2303e86e39d36f1ad0030
Anniken Qvam Strand <annikenqs@gmail.com>
2026-05-22 09:47:33 +0200

    ISSUE5: Feat: Implement routing for event page

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,78 @@
 import { useState } from "react";
 import { createPortal } from "react-dom";
+import {
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
+
 import "./App.css";
+
+import EventPage from "./pages/EventPage";
 import { CreateEventModal } from "./components/CreateEventModal/CreateEventModal";
 import { Modal } from "./components/Modal/Modal";
 
-function App() {
-  const [CreateEventModalOpen, setCreateEventModalOpen] = useState(false);
-  const [ModalOpen, setModalOpen] = useState(false);
+function HomePage() {
+  const navigate = useNavigate();
 
-  const handleButtonClick = () => {
+  const [createEventModalOpen, setCreateEventModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const closeModals = () => {
     setCreateEventModalOpen(false);
     setModalOpen(false);
   };
 
   return (
-    <>
-      <div className="App">
-        <button className="btn btn-open" onClick={() => setCreateEventModalOpen(true)}>
-          Open Create Event Modal
-        </button>
-        {CreateEventModalOpen &&
-          createPortal(
-            <CreateEventModal
-              closeModal={handleButtonClick}
-              onSubmit={handleButtonClick}
-            />,
-            document.body,
-          )}
-        <button className="btn btn-open" onClick={() => setModalOpen(true)}>
-          Open Simple Modal
-        </button>
-        {ModalOpen &&
-          createPortal(
-            <Modal closeModal={handleButtonClick} title={"Simple Modal"}>
-              <p>This is the content of the modal.</p>
-            </Modal>,
-            document.body,
-          )}
-      </div>
-    </>
+    <div className="App">
+      <button
+        className="btn btn-open"
+        onClick={() => setCreateEventModalOpen(true)}
+      >
+        Open Create Event Modal
+      </button>
+
+      <button
+        className="btn btn-open"
+        onClick={() => setModalOpen(true)}
+      >
+        Open Simple Modal
+      </button>
+
+      <button
+        className="btn btn-open"
+        onClick={() => navigate("/event")}
+      >
+        Go to Event Page
+      </button>
+
+      {createEventModalOpen &&
+        createPortal(
+          <CreateEventModal
+            closeModal={closeModals}
+            onSubmit={closeModals}
+          />,
+          document.body
+        )}
+
+      {modalOpen &&
+        createPortal(
+          <Modal closeModal={closeModals} title="Simple Modal">
+            <p>This is the content of the modal.</p>
+          </Modal>,
+          document.body
+        )}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/event" element={<EventPage />} />
+    </Routes>
   );
 }
 
